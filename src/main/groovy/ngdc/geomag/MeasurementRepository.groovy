128788990091dc888780703c095c9da558d8d796
John Cartwright <john.c.cartwright@noaa.gov>
2015-06-20 13:11:09 -0600

    add within query to repository

--- src/main/groovy/ngdc/geomag/MeasurementRepository.groovy
+++ src/main/groovy/ngdc/geomag/MeasurementRepository.groovy
@@ -1,7 +1,12 @@
 package ngdc.geomag
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.CrudRepository
+import com.vividsolutions.jts.geom.Geometry
+import org.springframework.data.jpa.repository.Query
 
 interface MeasurementRepository extends CrudRepository<Measurement, Long> {
     List<Measurement> findByDevice(String device);
+
+    @Query("select m from Measurement m where within(m.geom, ?1) = true")
+    List<Measurement> findWithin(Geometry filter)
 }
